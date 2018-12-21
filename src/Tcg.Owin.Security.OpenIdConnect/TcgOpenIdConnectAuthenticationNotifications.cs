8ecbe8a6b355e546665419c2775e67cb794cace4
Sander Rijken <s.rijken@competence.biz>
2018-12-21 22:30:53 +0100

    initialize notifications with dummy functions

--- src/Tcg.Owin.Security.OpenIdConnect/TcgOpenIdConnectAuthenticationNotifications.cs
+++ src/Tcg.Owin.Security.OpenIdConnect/TcgOpenIdConnectAuthenticationNotifications.cs
@@ -15,29 +15,29 @@ namespace Tcg.Owin.Security.OpenIdConnect
         // Summary:
         //     Invoked if exceptions are thrown during request processing. The exceptions will
         //     be re-thrown after this event unless suppressed.
-        public Func<AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions>, Task> AuthenticationFailed { get; set; }
+        public Func<AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions>, Task> AuthenticationFailed { get; set; } = notification => Task.FromResult(0);
         //
         // Summary:
         //     Invoked after security token validation if an authorization code is present in
         //     the protocol message.
-        public Func<AuthorizationCodeReceivedNotification, Task> AuthorizationCodeReceived { get; set; }
+        public Func<AuthorizationCodeReceivedNotification, Task> AuthorizationCodeReceived { get; set; } = notification => Task.FromResult(0);
         //
         // Summary:
         //     Invoked when a protocol message is first received.
-        public Func<MessageReceivedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions>, Task> MessageReceived { get; set; }
+        public Func<MessageReceivedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions>, Task> MessageReceived { get; set; } = notification => Task.FromResult(0);
         //
         // Summary:
         //     Invoked to manipulate redirects to the identity provider for SignIn, SignOut,
         //     or Challenge.
-        public Func<RedirectToIdentityProviderNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions>, Task> RedirectToIdentityProvider { get; set; }
+        public Func<RedirectToIdentityProviderNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions>, Task> RedirectToIdentityProvider { get; set; } = notification => Task.FromResult(0);
         //
         // Summary:
         //     Invoked with the security token that has been extracted from the protocol message.
-        public Func<SecurityTokenReceivedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions>, Task> SecurityTokenReceived { get; set; }
+        public Func<SecurityTokenReceivedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions>, Task> SecurityTokenReceived { get; set; } = notification => Task.FromResult(0);
         //
         // Summary:
         //     Invoked after the security token has passed validation and a ClaimsIdentity has
         //     been generated.
-        public Func<SecurityTokenValidatedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions>, Task> SecurityTokenValidated { get; set; }
+        public Func<SecurityTokenValidatedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions>, Task> SecurityTokenValidated { get; set; } = notification => Task.FromResult(0);
     }
 }
